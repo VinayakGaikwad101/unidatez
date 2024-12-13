c9f118b692c40163ed2f4bab4edb565e5dd48b46
VinayakGaikwad <vinaayakgaikwad@gmail.com>
2024-12-13 12:18:48 +0530

    bug fix 3.5

--- server/index.js
+++ server/index.js
@@ -3,17 +3,18 @@ import connectToMongoDB from "./db/mongodb.js";
 import dotenv from "dotenv";
 import preLaunchRouter from "./routes/PreLaunchRoute.js";
 import cors from "cors";
-import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const __dirname = path.resolve();
 const app = express();
-app.use(express.static(path.join(__dirname, "/client/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+app.use(
+  cors({
+    origin: "https://unidatez-bcoo.onrender.com/",
+    methods: "GET, POST, PUT, PATCH, DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/pre-register", preLaunchRouter); // http://localhost:3000/api/pre-register/save/email
