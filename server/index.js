33117417eba3028701af8ba50f59420b891c6827
VinayakGaikwad <vinaayakgaikwad@gmail.com>
2024-12-14 22:04:17 +0530

    fixed cors issue for deployments

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(
   cors({
-    origin: "www.unidatez.com",
+    origin: "https://www.unidatez.com",
     methods: "GET, POST, PUT, PATCH, DELETE",
     credentials: true,
   })
