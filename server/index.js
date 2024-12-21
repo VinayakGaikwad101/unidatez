0abe1afc5cb5ef45dca170a67fea4b996a4d1837
VinayakGaikwad <vinaayakgaikwad@gmail.com>
2024-12-21 08:51:46 +0530

    sub routing condifuration

--- server/index.js
+++ server/index.js
@@ -3,11 +3,17 @@ import connectToMongoDB from "./db/mongodb.js";
 import dotenv from "dotenv";
 import preLaunchRouter from "./routes/PreLaunchRoute.js";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(
   cors({
     origin:
@@ -21,6 +27,14 @@ app.use(
 app.use(express.json());
 
 app.use("/api/pre-register", preLaunchRouter); // http://localhost:3000/api/pre-register/save/email
+
+// Add this code to serve static files from the client folder
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// Catch-all route to serve index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 connectToMongoDB();
 
