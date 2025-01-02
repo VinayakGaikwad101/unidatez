eb43d1662919ba736af9f004d95ae7f1a1167adc
VinayakGaikwad <vinaayakgaikwad@gmail.com>
2025-01-02 19:54:04 +0530

    added background in all screens

--- client/src/pages/loggedUser/Match.jsx
+++ client/src/pages/loggedUser/Match.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Match = () => {
   return (
-    <div>Match</div>
-  )
-}
+    <div className="relative min-h-screen flex flex-col items-center justify-center overflow-hidden">
+      <img
+        src="/mobile_login_bg.jpg"
+        alt="Romantic background"
+        className="absolute inset-0 w-full h-full object-cover sm:hidden"
+      />
+      <img
+        src="/native_login_bg.jpg"
+        alt="Romantic background"
+        className="absolute inset-0 w-full h-full object-cover hidden sm:block"
+      />
+    </div>
+  );
+};
 
-export default Match
+export default Match;
